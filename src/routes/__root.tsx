33cfe10f2e1b42323f2c3c52373069285fa83972
Rey Trajano <rey@reytrajano.com>
2025-04-20 23:27:47 -0700

    initial 98.css

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,51 +4,52 @@ import {
   Outlet,
   Scripts,
   createRootRoute,
-} from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import * as React from 'react'
-import { DefaultCatchBoundary } from '~/components/DefaultCatchBoundary'
-import { NotFound } from '~/components/NotFound'
-import appCss from '~/styles/app.css?url'
-import { seo } from '~/utils/seo'
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import * as React from "react";
+import { DefaultCatchBoundary } from "~/components/DefaultCatchBoundary";
+import { NotFound } from "~/components/NotFound";
+import appCss from "~/styles/app.css?url";
+import { seo } from "~/utils/seo";
+import "98.css";
 
 export const Route = createRootRoute({
   head: () => ({
     meta: [
       {
-        charSet: 'utf-8',
+        charSet: "utf-8",
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
       },
       ...seo({
         title:
-          'TanStack Start | Type-Safe, Client-First, Full-Stack React Framework',
+          "TanStack Start | Type-Safe, Client-First, Full-Stack React Framework",
         description: `TanStack Start is a type-safe, client-first, full-stack React framework. `,
       }),
     ],
     links: [
-      { rel: 'stylesheet', href: appCss },
+      { rel: "stylesheet", href: appCss },
       {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png',
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
       },
       {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon-32x32.png',
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
       },
       {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/favicon-16x16.png',
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
       },
-      { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
-      { rel: 'icon', href: '/favicon.ico' },
+      { rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
+      { rel: "icon", href: "/favicon.ico" },
     ],
   }),
   errorComponent: (props) => {
@@ -56,18 +57,18 @@ export const Route = createRootRoute({
       <RootDocument>
         <DefaultCatchBoundary {...props} />
       </RootDocument>
-    )
+    );
   },
   notFoundComponent: () => <NotFound />,
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <RootDocument>
       <Outlet />
     </RootDocument>
-  )
+  );
 }
 
 function RootDocument({ children }: { children: React.ReactNode }) {
@@ -76,64 +77,85 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <head>
         <HeadContent />
       </head>
-      <body>
-        <div className="p-2 flex gap-2 text-lg">
+      <body className="grid grid-cols-[200px_auto]">
+        <aside className="p-2 h-svh">
+          <ul className="tree-view h-full">
+            <li>
+              <Link
+                to="/"
+                activeProps={{ className: "font-bold" }}
+                activeOptions={{ exact: true }}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/posts"
+                activeProps={{ className: "font-bold" }}
+                activeOptions={{ exact: true }}
+              >
+                Posts
+              </Link>
+            </li>
+          </ul>
+        </aside>
+        {/* <div className="p-2 flex gap-2 text-lg">
           <Link
             to="/"
             activeProps={{
-              className: 'font-bold',
+              className: "font-bold",
             }}
             activeOptions={{ exact: true }}
           >
             Home
-          </Link>{' '}
+          </Link>{" "}
           <Link
             to="/posts"
             activeProps={{
-              className: 'font-bold',
+              className: "font-bold",
             }}
           >
             Posts
-          </Link>{' '}
+          </Link>{" "}
           <Link
             to="/users"
             activeProps={{
-              className: 'font-bold',
+              className: "font-bold",
             }}
           >
             Users
-          </Link>{' '}
+          </Link>{" "}
           <Link
             to="/route-a"
             activeProps={{
-              className: 'font-bold',
+              className: "font-bold",
             }}
           >
             Pathless Layout
-          </Link>{' '}
+          </Link>{" "}
           <Link
             to="/deferred"
             activeProps={{
-              className: 'font-bold',
+              className: "font-bold",
             }}
           >
             Deferred
-          </Link>{' '}
+          </Link>{" "}
           <Link
             // @ts-expect-error
             to="/this-route-does-not-exist"
             activeProps={{
-              className: 'font-bold',
+              className: "font-bold",
             }}
           >
             This Route Does Not Exist
           </Link>
-        </div>
-        <hr />
-        {children}
+        </div> */}
+        <main className="max-h-svh overflow-auto">{children}</main>
         <TanStackRouterDevtools position="bottom-right" />
         <Scripts />
       </body>
     </html>
-  )
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,25 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Home,
-})
+});
 
 function Home() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!!!</h3>
+    <div className="h-svh flex justify-center items-center">
+      <div className="window w-72">
+        <div className="title-bar">
+          <div className="title-bar-text">Welcome!</div>
+          <div className="title-bar-controls">
+            <button aria-label="Help" />
+            <button aria-label="Close" />
+          </div>
+        </div>
+
+        <div className="window-body">
+          <p className="text-center">Welcome Home!</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
