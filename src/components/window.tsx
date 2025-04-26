1bbdb65f60432dc07f53139dbff22c0026184631
Rey Trajano <rey@reytrajano.com>
2025-04-26 11:45:01 -0700

    initial window centering

--- src/components/window.tsx
+++ src/components/window.tsx
@@ -1,17 +1,35 @@
 import React, { useState } from "react";
 import { Rnd } from "react-rnd";
+import { useWindowSize } from "usehooks-ts";
 import { cn } from "@/lib/utils";
 
 export interface WindowContainerProps
-  extends React.HTMLAttributes<HTMLDivElement> {}
+  extends React.HTMLAttributes<HTMLDivElement> {
+  initialHeight?: number;
+  initialWidth?: number;
+}
 
 const Container = React.forwardRef<HTMLDivElement, WindowContainerProps>(
-  ({ className, ...props }, ref) => {
-    const [state, setState] = useState({ x: 50, y: 50 });
+  ({ className, initialHeight = 300, initialWidth = 300, ...props }, ref) => {
+    const { width: windowWidth = 0, height: windowHeight = 0 } =
+      useWindowSize();
+    const initialX = Math.max((windowWidth - initialWidth) / 2, 0);
+    const initialY = Math.max((windowHeight - 48 - initialHeight) / 2, 0);
+
+    console.table({
+      windowWidth,
+      windowHeight,
+      initialWidth,
+      initialHeight,
+      initialX,
+      initialY,
+    });
+
+    const [state, setState] = useState({ x: initialX, y: initialY });
 
     return (
       <Rnd
-        size={{ width: 300, height: 300 }}
+        size={{ width: initialWidth, height: initialHeight }}
         position={{ x: state.x, y: state.y }}
         onDragStop={(_, data) => setState({ x: data.x, y: data.y })}
         dragHandleClassName="title-bar"
